de84bc20812bc89f85bfed7b972270bb42c6ad73
dalfriedrice <akshaykiet1993@gmail.com>
2021-11-22 01:40:13 +0530

    Update walletController.js

--- wallet-backend/app/controller/walletController.js
+++ wallet-backend/app/controller/walletController.js
@@ -23,23 +23,28 @@ exports.getWalletById = (req, res) => {
 
 exports.createWallet = (req, res) => {
     const name = req.body.wallet_name;
-    const balance = req.body.balance;
-    const date = new Date().toISOString().slice(0, 19).replace('T', ' ');
-    const sql = `INSERT INTO wallet (wallet_name, opening_bal, creation_date) VALUES ("${name}", "${balance}", "${date}")`;
-    db.query(sql, (err, data) => {
-        console.log('Data',data, err);
-        if (err) {
-            res.status(500).send({ 'error': `OOPS! Something went wrong : ${err}` });
-        }
-        res.json({
-            "status": "Nice! Wallet Successfully created",
-            "statusCode": 200,
-            "reponse": {
-                "id" : data.insertId,
-                "opening_bal" : req.body.balance,
-                "name" : req.body.wallet_name,
-                date
+    const balance = parseFloat(req.body.balance);
+    if (balance) {
+        const date = new Date().toISOString().slice(0, 19).replace('T', ' ');
+        const sql = `INSERT INTO wallet (wallet_name, opening_bal, creation_date) VALUES ("${name}", "${balance}", "${date}")`;
+        db.query(sql, (err, data) => {
+            if (err) {
+                res.status(500).send({ 'error': `OOPS! Something went wrong : ${err}` });
+            } else {
+                res.json({
+                    "status": "Nice! Wallet Successfully created",
+                    "statusCode": 200,
+                    "reponse": {
+                        "id": data.insertId,
+                        "opening_bal": req.body.balance,
+                        "name": req.body.wallet_name,
+                        date
+                    }
+                });
             }
         });
-    });
+    } else {
+        res.status(500).send({ 'error': "Incorrect Balance" });
+    }
+
 }
