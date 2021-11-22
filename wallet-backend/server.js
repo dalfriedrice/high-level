9a508b5677eb9d00c420fa718f1048cab793c711
dalfriedrice <akshaykiet1993@gmail.com>
2021-11-22 22:00:03 +0530

    Update server.js

--- wallet-backend/server.js
+++ wallet-backend/server.js
@@ -5,11 +5,20 @@ const transactionsRouter = require('./app/routes/transactions');
 
 const app = express();
 
-var corsOptions = {
-  origin: "http://localhost:8081"
-};
+var allowedDomains = ['https://wallet-frontend-heroku.herokuapp.com', 'http://localhost:8081'];
 
-app.use(cors(corsOptions));
+app.use(cors({
+  origin: function (origin, callback) {
+    // bypass the requests with no origin (like curl requests, mobile apps, etc )
+    if (!origin) return callback(null, true);
+ 
+    if (allowedDomains.indexOf(origin) === -1) {
+      var msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 // parse requests of content-type - application/json
 app.use(express.json());
